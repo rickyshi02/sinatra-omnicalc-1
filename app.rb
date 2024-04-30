ffe59f25e6d8b674b43d5015398aaa02c650e994
rickyshi02 <114941300+rickyshi02@users.noreply.github.com>
2024-04-30 22:30:32 +0000

    Pending changes exported from your codespace

--- app.rb
+++ app.rb
@@ -32,10 +32,23 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
-  @apr = (params.fetch("user_apr").to_f)/100
+  @apr = params.fetch("user_apr").to_f
+  @rate = @apr/100/12
   @years = params.fetch("user_years").to_i
   @pv=params.fetch("user_pv").to_f
-  @payment=(@apr*@pv)/((1-(1+@apr))**(-@years))
-  @form = [["APR", "#{@apr*100}%"], ["Number of years", @years], ["Principal", "$#{@pv}"], ["Payment", "$#{@payment}"]]
+  @payment=((@rate*@pv)/(1-(1+@rate)**(-@years*12))).to_fs(:currency)
+  @form = [["APR", @apr.to_fs(:percentage, { :precision => 4 } )], ["Number of years", @years], ["Principal", @pv.to_fs(:currency)], ["Payment", @payment]]
+  erb(:flexible)
+end
+
+get("/random/new") do
+  erb(:random_form)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @random= Random.rand(@min..@max)
+  @form = [["Minimum", @min], ["Maximum", @max], ["Random", @random]]
   erb(:flexible)
 end
